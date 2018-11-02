af1c4fada8a1c55fc4a5a0f72321df315e7896e1
renkeju <renkeju@renkejudeMBP.lan>
2018-11-02 17:39:40 +0800

    add docker compose build #4

--- README.md
+++ README.md
@@ -6,6 +6,28 @@
 
 ## Docker 容器部署
 
+1. 克隆目录
+
+    ```
+    git clone https://github.com/81192/kubernetes-docs.git
+    cd ./kubernetes-docs/docker/
+    ```
+
+2. 执行脚本
+
+    ```
+    # 如果linux系统中已安装 Docker 与 docker-compose，请跳过此命令，否则执行。
+    bash build.sh set
+    # 构建镜像
+    bash build.sh make
+    ```
+
+3. 启动容器
+
+   ```
+   docker-compose up -d
+   ```
+
 > 参考
   1. [kubespray][1]
   2. [ansible 权威指南][2]
--- docker/build.sh
+++ docker/build.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
-set -x 
-
+## set docker 
+function set_docker () {
 ## install docker
-
 curl -sSL https://get.daocloud.io/docker | sh
 
 ## install docker-compose
-
 [ -f /usr/local/bin/docker-compose ] || \
 curl -L https://get.daocloud.io/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose
 
 
 ## use daocloud docker registy 
-
 if [ ! -f /etc/docker/daemon.json ]; then
 	curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
 	/usr/bin/systemctl restart docker
 fi
+}
 
+function make_compose () {
 ## local ENV setting
-
 SPHINX_DIR=$(dirname "$PWD")
 HTML_DIR=$(find ${SPHINX_DIR} -name index.html -exec dirname {} \;)
 
@@ -32,6 +30,7 @@ services:
 
   html:
     container_name: sphinx-kubernetes
+    image: sphinx:1.6.4-apline3.8
     build:
       context: .
       dockerfile: Dockerfile.sphinx
@@ -49,5 +48,18 @@ services:
 EOF
 
 ## run container
+cd ${SPHINX_DIR}/docker/ && docker-compose -f docker-compose.yaml build
+}
 
-cd ${SPHINX_DIR}/docker/ && docker-compose -f docker-compose.yaml up -d
+case $1 in
+	set)
+	set_docker
+	;;
+	make)
+	make_compose
+	;;
+	*)
+	echo "Usage: $0 {set|make}"
+        exit 1
+	;;
+esac
