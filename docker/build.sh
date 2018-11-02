e5014556ce741e76d3a76a39f9689182cfabd88e
root <root@remote.renkeju.com>
2018-11-02 17:05:25 +0800

    add docker compose build #2

--- docker/build.sh
+++ docker/build.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 
+set -x 
+
 ## install docker
 
 curl -sSL https://get.daocloud.io/docker | sh
 
 ## install docker-compose
 
-curl -L https://get.daocloud.io/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+[ -f /usr/local/bin/docker-compose ] || \
+curl -L https://get.daocloud.io/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && \
 chmod +x /usr/local/bin/docker-compose
+
 
 ## use daocloud docker registy 
 
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
-/usr/bin/systemctl restart docker
+if [ ! -f /etc/docker/daemon.json ]; then
+	curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+	/usr/bin/systemctl restart docker
+fi
 
 ## local ENV setting
 
 SPHINX_DIR=$(dirname "$PWD")
-HTML_DIR=$(find ~/Documents/kubernetes-docs -name index.html -exec dirname {} \;)
+HTML_DIR=$(find ${SPHINX_DIR} -name index.html -exec dirname {} \;)
 
 ## write docker compose
 cat << EOF > docker-compose.yaml
@@ -44,5 +50,4 @@ EOF
 
 ## run container
 
-cd ${SPHINX_DIR}/docker/
-docker-compose -f docker-compose.yaml up -d
+cd ${SPHINX_DIR}/docker/ && docker-compose -f docker-compose.yaml up -d
